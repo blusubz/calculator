bc31d094fad73fc646d6cb86c0f3245662b147e2
Morris <underwater_forests@outlook.com>
2026-04-25 21:11:49 -0400

    Feat: Digit buttons stored in JS variables

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
-function add() {
-    return 2 + 1;
+let num1 = 0;
+let num2 = 0;
+let operator = "";
+
+
+function add(a, b) {
+    return a + b;
 }
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    return 0;
+}
+
+function digitPressed() {
+    return 0;
+}
+
+let digitBtn = document.querySelector('.digitBtn');
+
+digitBtn.addEventListener('click', (event) => {
+    // if button is clicked
+    if (event.target.tagName === 'BUTTON') {
+        const digit = event.target.value;
+        num1 = digit;
+    } 
+    console.log(num1);
+});
